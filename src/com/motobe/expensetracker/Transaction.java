b24b251fa1185406e9630bc3a06ae5b8d44514d4
Mazi Tobe <emma.too50@googlemail.com>
2024-02-05 20:47:59 +0100

    updates

---
+++ src/com/motobe/expensetracker/Transaction.java
@@ -0,0 +1,11 @@
+package com.motobe.expensetracker;
+
+public class Transaction {
+    protected double amount;
+    protected String transactionID;
+    protected int transactionDay;
+    protected int transactionMonth;
+    protected int transactionYear;
+    protected String transactionDescription;
+    protected String transactionCategory;
+}
